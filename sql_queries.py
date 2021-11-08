c33a179017d3bfd39b0f26fa3e4673ab599b85fc
Harvey Mitchell <85601895+techmitchh@users.noreply.github.com>
2021-11-08 17:21:45 -0600

    refactor: Drop Table syntax added in sql_queries

--- sql_queries.py
+++ sql_queries.py
@@ -7,13 +7,13 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS user"
+song_table_drop = "DROP TABLE IF EXISTS song"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
@@ -23,7 +23,7 @@ staging_events_table_create= ("""
 staging_songs_table_create = ("""
 """)
 
-songplay_table_create = ("""
+songplay_table_create = (""" 
 """)
 
 user_table_create = ("""

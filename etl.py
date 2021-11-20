a4c11792f545984d0e52a376ef722c4668724e78
Harvey Mitchell <85601895+techmitchh@users.noreply.github.com>
2021-11-20 09:39:38 -0600

    doc: Adding docstrings to load_staging_tables, insert_tables

--- etl.py
+++ etl.py
@@ -4,18 +4,34 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Loads S3 data into Staging_Events table and Staging_Songs table
+
+    Arg:
+        cur: executes copy queries; staging_events_copy and staging_stongs_copy 
+        conn: commits the executed query
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Takes staging tables data and inserts into the following tables: songplay, users, artists, songs, time.
+
+    Arg:
+        cur: executes insert queries; songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert 
+        conn: commits the executed queries
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
-
 def main():
+    """
+    Connects to Redshift Cluster and Database, then executes the load_staging_tables and insert_tables functions
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
